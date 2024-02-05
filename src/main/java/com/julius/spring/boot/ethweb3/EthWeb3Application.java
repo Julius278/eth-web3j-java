1f4a672237e21f1f8ea3960535dd1dd8228b36b0
Julius Lauterbach <Julius.Lauterbach@nttdata.com>
2024-02-05 10:15:45 +0100

    initial empty spring boot application

---
+++ src/main/java/com/julius/spring/boot/ethweb3/EthWeb3Application.java
@@ -0,0 +1,13 @@
+package com.julius.spring.boot.ethweb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthWeb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthWeb3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/julius/spring/boot/ethweb3/EthWeb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julius.spring.boot.ethweb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthWeb3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
