25deb64a65bd4339d4a1e892de87a1feeda9e7a3
Julius Lauterbach <Julius.Lauterbach@nttdata.com>
2024-03-13 23:40:04 +0100

    adding toString annotation in TransferRequest model

--- src/main/java/com/julius/spring/boot/ethweb3/service/TransferRequest.java
+++ src/main/java/com/julius/spring/boot/ethweb3/service/TransferRequest.java
@@ -2,11 +2,13 @@ package com.julius.spring.boot.ethweb3.service;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@ToString
 public class TransferRequest {
     private String privateKey;
     private String recipient;
