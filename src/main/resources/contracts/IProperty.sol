b51c7ef2cd513867cbde3579c5fefc4cff9f8da0
Julius Lauterbach <git@juliuslauterbach.de>
2026-07-28 00:05:31 +0200

    update Solidity version to 0.8.15; adjust package.json and pom.xml for compatibility with new version

--- src/main/resources/contracts/IProperty.sol
+++ src/main/resources/contracts/IProperty.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity 0.8.15;
 
 interface IProperty {
     function setValue(int256 _value) external;
@@ -9,3 +9,4 @@ interface IProperty {
     function setName(string memory _name) external;
     function getName() external view returns (string memory);
 }
+
